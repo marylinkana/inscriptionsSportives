1cfb945e0804630171355ec0cc2b0f1f272706be
antoine szajryck <antoine.szajryck@gmail.com>
2018-11-16 14:55:40 +0100

    public boolean inscriptionsOuvertes()

--- InscriptionsSportives/src/inscriptions/Competition.java
+++ InscriptionsSportives/src/inscriptions/Competition.java
@@ -58,9 +58,9 @@ public class Competition implements Comparable<Competition>, Serializable
 
 	public boolean inscriptionsOuvertes()
 	{
-		// TODO retourner vrai si et seulement si la date système est antérieure à la date de clôture.
-		return true;
+		return this.dateCloture.isAfter( LocalDate.now() );
 	}
+
 
 	/**
 	 * Retourne la date de cloture des inscriptions.
