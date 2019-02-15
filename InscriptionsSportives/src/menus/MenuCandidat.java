aac1622bc64f64d8382fd0c07ab0db16c8fcaa74
marylinkana <43268881+marylinkana@users.noreply.github.com>
2019-02-15 11:49:50 +0100

    Link with CommandLine

--- InscriptionsSportives/src/menus/MenuCandidat.java
+++ InscriptionsSportives/src/menus/MenuCandidat.java
@@ -1,5 +1,27 @@
 package menus;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.SortedSet;
+
+import commandLineMenus.*;
+import commandLineMenus.examples.ListOptions;
+import commandLineMenus.rendering.examples.util.InOut;
+import inscriptions.Candidat;
+import inscriptions.Competition;
+import inscriptions.Equipe;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
+
 public class MenuCandidat {
 
+
+		public MenuCandidat()
+		{
+			
+		}
+
 }
+
+
