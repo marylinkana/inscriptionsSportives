7b29211948be1a25ab4d1b1a34fe2c99631f69b6
marylinkana <43268881+marylinkana@users.noreply.github.com>
2019-03-01 15:45:59 +0100

    Adaptation des menus

--- InscriptionsSportives/src/menus/MenuPersonne.java
+++ InscriptionsSportives/src/menus/MenuPersonne.java
@@ -1,5 +1,60 @@
 package menus;
 
+import java.util.SortedSet;
+
+import commandLineMenus.Action;
+import commandLineMenus.Menu;
+import commandLineMenus.Option;
+import commandLineMenus.rendering.examples.util.InOut;
+import inscriptions.Inscriptions;
+import inscriptions.Personne;
+
 public class MenuPersonne {
 
+//	public static Menu getPersonneMenu()
+//	{
+//		Menu personneMenu = new Menu("Personne Sub-Menu", "Personne", "per");
+//		return personneMenu;
+//	}
+//	
+//	
+//	getPersonneMenu().add(affichePersonneOption());
+//	getPersonneMenu().add(creerPersonneOption());
+//	
+//	getPersonneMenu().addBack("r");
+//	getPersonneMenu().setAutoBack(true);
+//	
+//	
+//	private static  Option affichePersonneOption() {
+//		return new Option("Afficher les personnes", "afpers", new Action()
+//		{
+//			public void optionSelected()
+//			{
+//				System.out.println("Liste des personnes : ");
+//        		Inscriptions inscriptions = Inscriptions.getInscriptions();
+//				SortedSet<Personne> lesPersonnes = inscriptions.getPersonnes();
+//				System.out.println( lesPersonnes );
+//				
+//			}
+//		});
+//	}
+//	
+//	
+//	
+//	
+//	private static  Option creerPersonneOption() {
+//		return new Option("créer une personne", "crpers", new Action()
+//		{
+//			public void optionSelected()
+//			{
+//				System.out.println("Enter le nom, le prenom et l'adresse email de la personne que vous voulez ajouter ");
+//				String nom = InOut.getString("Nom : ");
+//				String prenom = InOut.getString("Prénom : ");
+//                String mail = InOut.getString("Email : ");
+//        		Inscriptions inscriptions = Inscriptions.getInscriptions();
+//                Personne personne = inscriptions.createPersonne(nom, prenom, mail);
+//			}
+//		});
+//	}
+//
 }
